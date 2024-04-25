20bea2f99a2f85ca9adf1d98acc4b36f7f02f797
Andrea Windisch <andrea.windisch@gmx.net>
2024-04-25 10:33:52 +0200

    dialogField is displayed in terminal with variable text

--- libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
+++ libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af22f2ebc49be539d50078afa7a9eff3029b3a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52f2ef3ae33eb8421e7e22bf4e97c9444b0e75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("libs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
